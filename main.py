80a5a430a30e3143ca676a2580183f22aa89835c
KaiRenken <kai.renken@gmx.de>
2024-10-28 13:18:32 +0100

    implement simple tutorial

--- main.py
+++ main.py
@@ -1,73 +1,59 @@
-import matplotlib.pyplot as plt
-import pandas as pd
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from torch import nn
 
 
-class Model(nn.Module):
-
-    def __init__(self, in_features=4, h1=8, h2=9, out_features=3, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fc1 = nn.Linear(in_features, h1)
-        self.fc2 = nn.Linear(h1, h2)
-        self.out = nn.Linear(h2, out_features)
+class MyMachine(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(2, 5),
+            nn.ReLU(),
+            nn.Linear(5, 1)
+        )
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.out(x))
-
+        x = self.fc(x)
         return x
 
 
-torch.manual_seed(41)
+def get_dataset():
+    X = torch.rand((1000, 2))
+    x1 = X[:, 0]
+    x2 = X[:, 1]
+    y = x1 * x2
+    return X, y
 
-model = Model()
 
-url = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
-my_df = pd.read_csv(url)
+def train():
+    model = MyMachine()
+    model.train()
+    X, y = get_dataset()
+    NUM_EPOCHS = 1000
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-5)
+    criterion = torch.nn.MSELoss(reduction='mean')
 
-my_df['species'] = my_df['species'].replace('setosa', 0.0)
-my_df['species'] = my_df['species'].replace('versicolor', 1.0)
-my_df['species'] = my_df['species'].replace('virginica', 2.0)
+    for epoch in range(NUM_EPOCHS):
+        optimizer.zero_grad()
+        y_pred = model(X)
+        y_pred = y_pred.reshape(1000)
+        loss = criterion(y_pred, y)
+        loss.backward()
+        optimizer.step()
+        print(f'Epoch:{epoch}, Loss:{loss.item()}')
+    torch.save(model.state_dict(), 'model.h5')
 
-X = my_df.drop('species', axis='columns')
-y = my_df['species']
 
-X = X.values
-y = y.values
+def test():
+    model = MyMachine()
+    model.load_state_dict(torch.load("model.h5"))
+    model.eval()
+    X, y = get_dataset()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
+    with torch.no_grad():
+        y_pred = model(X)
+        print(r2_score(y, y_pred))
 
-X_train = torch.FloatTensor(X_train)
-X_test = torch.FloatTensor(X_test)
 
-y_train = torch.LongTensor(y_train)
-y_test = torch.LongTensor(y_test)
-
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
-epochs = 100
-losses = []
-
-for i in range(epochs):
-    y_pred = model.forward(X_train)
-
-    loss = criterion(y_pred, y_train)
-    losses.append(loss.detach().numpy())
-
-    if i % 10 == 0:
-        print(f'Epoch: {i} and loss: {loss}')
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-plt.plot(range(epochs), losses)
-plt.ylabel('loss/error')
-plt.xlabel('Epoch')
-plt.show()
+train()
+test()
