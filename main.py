bc61150a5ae3eca6e8457aad07036da53453d60b
KaiRenken <kai.renken@gmx.de>
2024-10-30 14:35:04 +0100

    train model to add two numbers

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from sklearn.metrics import r2_score
 from torch import nn
 
 ARCH = nn.Sequential(
-    nn.Linear(2, 5),
+    nn.Linear(2, 10),
     nn.ReLU(),
-    nn.Linear(5, 1)
+    nn.Linear(10, 10),
+    nn.ReLU(),
+    nn.Linear(10, 1)
 )
 
 NUM_EPOCHS = 1000
@@ -16,10 +18,10 @@ SIZE_TEST_SET = 1000
 
 
 def get_dataset(size):
-    X = torch.rand((size, 2))
+    X = torch.rand((size, 2)) * 100
     x1 = X[:, 0]
     x2 = X[:, 1]
-    y = x1 * x2
+    y = x1 + x2
     return X, y
 
 
@@ -36,9 +38,10 @@ class MyMachine(nn.Module):
 
 def train():
     model = MyMachine()
+    model.load_state_dict(torch.load("model.h5"))
     model.train()
     X, y = get_dataset(SIZE_TRAIN_SET)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-6, weight_decay=1e-5)
     criterion = torch.nn.MSELoss(reduction='mean')
 
     for epoch in range(NUM_EPOCHS):
@@ -60,9 +63,17 @@ def test():
 
     with torch.no_grad():
         y_pred = model(X)
-        print(y_pred)
         print(r2_score(y, y_pred))
 
 
-train()
-test()
+# train()
+# test()
+
+n1 = float(input())
+n2 = float(input())
+
+model = MyMachine()
+model.load_state_dict(torch.load("model.h5"))
+model.eval()
+
+print(model(torch.Tensor([[n1, n2]])))
