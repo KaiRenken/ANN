e50038eb1f4ca6bcb1040a6d01471df35ba22e43
KaiRenken <kai.renken@gmx.de>
2024-11-11 09:13:26 +0100

    add digit_recognition.py

--- convolution_playground.py
+++ convolution_playground.py
@@ -4,21 +4,18 @@ from PIL import Image
 from numpy import asarray
 from scipy import signal
 
-image = Image.open('test_image_bw.jpg')
+image = Image.open('test_me_bw.jpg')
 image_data = asarray(image)
 
-filter_kernel = np.array([[1, 0, 0, 0, -1],
-                          [1, 0, 0, 0, -1],
-                          [1, 0, 0, 0, -1],
-                          [1, 0, 0, 0, -1],
-                          [1, 0, 0, 0, -1]])
+a = np.random.rand(3, 6)
+b = -a
+filter_kernel = np.concatenate((a, b)) * 2
+np.random.shuffle(filter_kernel)
 
-# filter_kernel = filter_kernel.transpose()
+processed_image_data = image_data
 
-processed_image_data = signal.convolve2d(image_data, filter_kernel, mode='same', boundary='symm')
+for i in range(1):
+    processed_image_data = signal.convolve2d(processed_image_data, filter_kernel, mode='same', boundary='symm')
 
-images = [image_data, processed_image_data]
-
-for i in range(2):
-    plt.imshow(images[i], cmap='gray', vmin=0, vmax=255)
-    plt.show()
+plt.imshow(processed_image_data, cmap='gray', vmin=0, vmax=255)
+plt.show()
